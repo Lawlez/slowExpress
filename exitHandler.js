dc5f2392156905de5018972d2aa09b669cdd0c9f
Dominik Feger <dk.feger@gmail.com>
2021-10-06 15:25:22 +0200

    improve logging and security

---
+++ exitHandler.js
@@ -0,0 +1,72 @@
+const { Logger, Human, Filter, Transform } = require("caterpillar");
+const { createWriteStream } = require("fs");
+class AppendLines extends Transform {
+  format(entry) {
+    //makes sure that we have newlines for each entry
+    return `${JSON.stringify(entry)} \n`;
+  }
+}
+
+const logger = new Logger();
+
+const initLogging = () => {
+  const today = new Date().toISOString().slice(0, 13); //2021-10-01T15
+  // Output human readable output to stdout in dev mode
+  if (process.env.NODE_ENV !== "production") {
+    logger.pipe(new Human()).pipe(process.stdout);
+  }
+  // Output all entries into a log file & human readable filtered logs to another log
+  logger
+    .pipe(new AppendLines())
+    .pipe(createWriteStream("./verbose-" + today + ".log"));
+  logger
+    .pipe(new Filter({ filterLevel: 7 }))
+    .pipe(new Human())
+    .pipe(createWriteStream("./human-" + today + ".log"));
+};
+
+const exitHandler = (options, exitCode, type) => {
+  logger.log(
+    "App is recieved signal of type " + type + " with error code: " + exitCode
+  );
+
+  if (type === "SIGINT") {
+    logger.log("App has recieved shutdown request by user");
+  }
+  if (options.exit) {
+    logger.info("exiting after " + Math.floor(process.uptime()) + " Seconds..");
+    logger.info("current resource usage: ", process.resourceUsage());
+    setTimeout(() => process.exit(exitCode), 2000);
+  }
+};
+
+const noOp = () => {
+  // keeps process running even on err
+  process.stdin.resume();
+  // do something when app is closing - on app shutdown i.e. process.exit()
+  process.on("exit", () => {
+    logger.warn("server shutting down");
+    exitHandler({ exit: true, logLevel: 5 }, 0);
+  });
+  // catches ctrl+c event
+  process.on(
+    "SIGINT",
+    exitHandler.bind(null, { exit: true, logLevel: 5 }, 0, "SIGINT")
+  );
+  // catches uncaught exceptions
+  process.on("uncaughtException", (err) => {
+    logger.error(err);
+    exitHandler({ exit: true, logLevel: 0 }, 1, "uncaughtException");
+  });
+  // catches unhandled promise rejections
+  process.on("unhandledRejection", (err) => {
+    //logger.error(err);
+    exitHandler({ exit: true, logLevel: 0 }, 1, "UnhandledPromiseRejection");
+  });
+};
+
+module.exports = {
+  noOp,
+  initLogging,
+  logger,
+};
--- slowExpress.js
+++ slowExpress.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const app = express();
-const fs = require("fs");
-const util = require("util");
-const log_file = fs.createWriteStream(
-  __dirname + "/node-" + Math.floor(Math.random() * 10000) + ".log",
-  { flags: "w" }
-);
+const session = require('express-session')
+const helmet = require('helmet')
+const { initLogging, logger, noOp } = require("./exitHandler");
 
-const log_stdout = process.stdout;
+//starts the Logging service & exithandler
+initLogging();
+noOp();
+
 const port = process.env.PORT || 8080;
 
 try {
+  app.use(helmet())
+  app.set('trust proxy', 1)
+  app.use(session({
+    secret: 'slow dude :)',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  }))
+  app.disable('x-powered-by')
+
   app.get("/", (req, res, next) => {
     res.setHeader("Content-Type", "text/html; charset=utf-8");
     res.setHeader("Transfer-Encoding", "chunked");
@@ -18,7 +28,7 @@ try {
     res.removeHeader('x-powered-by');
     res.removeHeader('server');
     const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-    console.log(
+    logger.log(
       "CONN: ",
       "host and path: ",
       `${req.hostname}${req.url}`,
@@ -27,22 +37,22 @@ try {
       "IP addr: ",
       ip
     );
-    console.log("FullInfo: ", req, next, req.headers.toString());
+    //logger.log("FullInfo: ", req, next, req.headers.toString());
     sendAndSleep(res, 1);
   });
 
   app.listen(port, () => {
-    console.log(`slowExpress listening at http://localhost:${port}`);
+    logger.log(`slowExpress listening at http://localhost:${port}`);
   });
 } catch (err) {
-  console.log(err);
+  logger.error(err);
 }
 
 const content =
   ' HTTP is a transfer protocol used by the World Wide Web to retrieve information from distributed servers. The HTTP model is extremely simple; the client establishes a connection to the remote server, then issues a request. The server then processes the request, returns a response, and closes the connection. \nThe request format for HTTP is quite simple. The first line specifies an object, together with the name of an object to apply the method to. The most commonly used method is "GET", which ask the server to send a copy of the object to the client. The client can also send a series of optional headers; these headers are in RFC-822 format. The most common headers are "Accept", which tells the server which object types the client can handle, and "User-Agent", which gives the implementation name of the client.\n The response format is also quite simple. Responses start with a status line indicating which version of HTTP the server is running, together with a result code and an optional message. This is followed by a series of optional object headers; the most important of these are "Content-Type", which describes the type of the object being returned, and "Content-Length", which indicates the length. The headers are teminated by an empty line. The server now sends any requested data. After the data have been sent, the server drops the connection. ';
 
 const sendAndSleep = (response, counter) => {
-  try {
+
     if (counter > content.length) {
       response.end();
     } else {
@@ -52,13 +62,6 @@ const sendAndSleep = (response, counter) => {
         sendAndSleep(response, counter);
       }, 3000);
     }
-  } catch (err) {
-    console.log(err);
-  }
+
 };
 
-console.log = function (d) {
-  //console log to file
-  log_file.write(util.format(d) + "\n");
-  log_stdout.write(util.format(d) + "\n");
-};
